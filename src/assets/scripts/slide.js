23ac1cd64f57b4f3783a889b8ab860ed49ef3ea3
Gustav Eén <gustaveen@gmail.com>
2014-09-23 13:14:30 +0200

    been workin with da scrolling header yo

--- src/assets/scripts/slide.js
+++ src/assets/scripts/slide.js
@@ -1,12 +1,52 @@
 var height = $(window).height();
 var width = $(window).width();
+var $header = $('.header');
 
 (function() {
   $(window).resize(function() {
-    $('.slide').height(height);
-    $('.slide').width(width);
-
+    $header.height(height);
+    $header.width(width);
     height = $(window).height();
     width = $(window).width();
+  });
+
+  function preventDefault(e) {
+    e = e || window.event;
+    if (e.preventDefault) {
+      e.preventDefault();
+    }
+    e.returnValue = false;
+  }
+  function wheel(e) {
+    preventDefault(e);
+  }
+
+  function disableScroll() {
+    if (window.addEventListener) {
+      window.addEventListener('DOMMouseScroll', wheel, false);
+    }
+    window.onmousewheel = document.onmousewheel = wheel;
+  }
+
+  function enableScroll() {
+    if (window.removeEventListener) {
+      window.removeEventListener('DOMMouseScroll', wheel, false);
+    }
+    window.onmousewheel = document.onmousewheel = null;
+  }
+
+  $(window).scroll(function() {
+    //console.log($(window).scrollTop());
+    $header.bind('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd', function() {
+      enableScroll();
+    });
+    if ($(window).scrollTop() >= 5 && $header.hasClass('visible')) {
+      $header.addClass('active').removeClass('visible');
+      disableScroll();
+    }
+    if ($header.hasClass('active') && $(window).scrollTop() === 0) {
+      $header.removeClass('active').addClass('visible');
+      $('.wrapper').removeClass('active');
+    }
   });
 }());
