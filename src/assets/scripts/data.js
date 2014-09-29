75886f8eada9752d780eb3e85f14287bdcab1383
Pierre Sköldborg <pierre.skoldborg@gmail.com>
2014-09-29 16:24:39 +0200

    New background image & erica placeholders

--- src/assets/scripts/data.js
+++ src/assets/scripts/data.js
@@ -7,43 +7,43 @@ var Data = Data || (function() {
       {
         name: 'Erica Liljeblad',
         email: 'erica[at]kommode[dot]se',
-        img: 'assets/images/mongo.png',
+        img: 'assets/images/erica.jpg',
         slogan: 'Geek'
       },
       {
         name: 'Pierre Sköldborg',
         email: 'pierre[at]kommode[dot]se',
-        img: 'assets/images/mongo.png',
+        img: 'assets/images/erica.jpg',
         slogan: 'Kaffe'
       },
       {
         name: 'Lars-Haakon Simonsen',
         email: 'lh[at]kommode[dot]se',
-        img: 'assets/images/mongo.png',
+        img: 'assets/images/erica.jpg',
         slogan: 'Gutt'
       },
       {
         name: 'Gustav Wallner',
         email: 'gustav.wallner[at]kommode[dot]se',
-        img: 'assets/images/mongo.png',
+        img: 'assets/images/erica.jpg',
         slogan: 'Skägg'
       },
       {
         name: 'Gustav Éen',
         email: 'gustav.een[at]kommode[dot]se',
-        img: 'assets/images/mongo.png',
+        img: 'assets/images/erica.jpg',
         slogan: 'Keps'
       },
       {
         name: 'Gabriella Jonsson',
         email: 'gabriella[at]kommode[dot]se',
-        img: 'assets/images/mongo.png',
+        img: 'assets/images/erica.jpg',
         slogan: 'Röd'
       },
       {
         name: 'Martina Milisic',
         email: 'martina[at]kommode[dot]se',
-        img: 'assets/images/mongo.png',
+        img: 'assets/images/erica.jpg',
         slogan: 'Borta'
       },
       {
