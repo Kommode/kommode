b86c1752a012120711b5d552d6db1caf4dd29773
Pierre Sköldborg <pierre.skoldborg@gmail.com>
2014-10-01 15:50:37 +0200

    Add images

--- src/assets/scripts/data.js
+++ src/assets/scripts/data.js
@@ -5,52 +5,52 @@ var Data = Data || (function() {
     pitch: 'blablablablabla',
     people: [
       {
+        name: 'Gustav Wallner',
+        email: 'gustav.wallner[at]kommode[dot]se',
+        img: 'assets/images/gustavW.jpg',
+        slogan: 'Skägg'
+      },
+      {
         name: 'Erica Liljeblad',
         email: 'erica[at]kommode[dot]se',
         img: 'assets/images/erica.jpg',
         slogan: 'Geek'
       },
       {
-        name: 'Pierre Sköldborg',
-        email: 'pierre[at]kommode[dot]se',
-        img: 'assets/images/erica.jpg',
-        slogan: 'Kaffe'
-      },
-      {
-        name: 'Lars-Haakon Simonsen',
-        email: 'lh[at]kommode[dot]se',
-        img: 'assets/images/erica.jpg',
-        slogan: 'Gutt'
-      },
-      {
-        name: 'Gustav Wallner',
-        email: 'gustav.wallner[at]kommode[dot]se',
-        img: 'assets/images/erica.jpg',
-        slogan: 'Skägg'
-      },
-      {
         name: 'Gustav Éen',
         email: 'gustav.een[at]kommode[dot]se',
-        img: 'assets/images/erica.jpg',
+        img: 'assets/images/gustavE.jpg',
         slogan: 'Keps'
       },
       {
         name: 'Gabriella Jonsson',
         email: 'gabriella[at]kommode[dot]se',
-        img: 'assets/images/erica.jpg',
+        img: 'assets/images/gabriella.jpg',
         slogan: 'Röd'
+      },
+      {
+        name: 'Pierre Sköldborg',
+        email: 'pierre[at]kommode[dot]se',
+        img: 'assets/images/pierre.jpg',
+        slogan: 'Kaffe'
       },
       {
         name: 'Martina Milisic',
         email: 'martina[at]kommode[dot]se',
-        img: 'assets/images/erica.jpg',
+        img: 'assets/images/martina.jpg',
         slogan: 'Borta'
       },
       {
         name: 'Andreas Sundström',
         email: 'andreas[at]kommode[dot]se',
-        img: 'assets/images/silouette.jpg',
+        img: 'assets/images/andreas.jpg',
         slogan: 'Helstressad'
+      },
+      {
+        name: 'Lars-Haakon Simonsen',
+        email: 'lh[at]kommode[dot]se',
+        img: 'assets/images/lars.jpg',
+        slogan: 'Gutt'
       }
     ]
   };
